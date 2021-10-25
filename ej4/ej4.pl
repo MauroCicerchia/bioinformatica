66e01da600779d6f1d90f328048ef9c8444ae9c8
Mauro Cicerchia <maurocicerchia98@gmail.com>
2021-10-25 17:46:23 -0300

    feat: ej4

---
+++ ej4/ej4.pl
@@ -0,0 +1,23 @@
+use warnings;
+use Bio::SeqIO;
+use Bio::SearchIO;
+use Bio::DB::GenPept;
+
+$blastFile = Bio::SearchIO->new(-file => 'blast.report', -format => 'blast');
+$pattern = $ARGV[0] || die "Usage: $0 <pattern>\n";
+$gp = Bio::DB::GenPept->new(-format => 'fasta');
+$outputFile = Bio::SeqIO->new(-file => '>results.fas', -format => 'fasta');
+
+while($currentResult = $blastFile->next_result) {
+    while($hit = $currentResult->next_hit) {
+        if($hit->description =~ /$pattern/gi) {
+            $seq = $gp->get_Seq_by_acc($hit->accession);
+            $outputFile->write_seq($seq);
+        }
+    }
+}
+
+sub getAccession {
+    $accessionEnd = index($_[0], '.');
+    return substr($_[0], 0, $accessionEnd);
+}
